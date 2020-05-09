80618ae8159b4de71503b2628766d00a1846e5fc
Kunal <sonee069@gmail.com>
2020-05-09 15:44:59 +0530

    test2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,9 +12,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1eac16572634aae6bef1840342ba96d4",
-"/": "1eac16572634aae6bef1840342ba96d4",
-"main.dart.js": "1b51b27318b34456d7dcdbb1b9765287",
+"index.html": "6362ed424057d279c1626bee81b65096",
+"/": "6362ed424057d279c1626bee81b65096",
+"main.dart.js": "c471db56fe71854ef7ecee3d85d60911",
 "manifest.json": "7bcddd3d68d57543e3cebd216b2cbdd5"
 };
 
